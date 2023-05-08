9fd919b62105cb4a615f1630a49fdf85d318e5c6
AryanMan12 <aryanaryan1212.ag@gmail.com>
2023-05-08 14:13:13 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:exercise_tracker/widgets/home_card.dart';
 import 'package:flutter/material.dart';
-import 'utils/colors.dart';
+import 'package:exercise_tracker/screens/homeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,39 +20,5 @@ class MyApp extends StatelessWidget {
         title: 'Fitness Tracker',
       ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: secBackgroundColor,
-        appBar: AppBar(
-          title: Text(widget.title),
-          backgroundColor: Colors.black,
-          elevation: 1,
-        ),
-        body: ListView(
-          children: const [
-            HomeCard(cardText: "Add Today's Data"),
-            SizedBox(
-              height: 5,
-            ),
-            HomeCard(cardText: "View Data"),
-            SizedBox(
-              height: 5,
-            ),
-            HomeCard(cardText: "Add Meta Data"),
-          ],
-        ));
   }
 }
---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:exercise_tracker/utils/colors.dart';
+import 'package:exercise_tracker/widgets/home_card.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: secBackgroundColor,
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Colors.black,
+          elevation: 1,
+        ),
+        body: ListView(
+          children: const [
+            HomeCard(cardText: "Add Today's Data"),
+            SizedBox(
+              height: 5,
+            ),
+            HomeCard(cardText: "View Data"),
+            SizedBox(
+              height: 5,
+            ),
+            HomeCard(cardText: "Add Meta Data"),
+          ],
+        ));
+  }
+}
